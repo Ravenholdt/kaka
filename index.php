5780b5c02cb1d0134aa0ca44877dbe9c755364ae
Ravenholdt <joppe4899@hotmail.com>
2025-03-31 22:32:20 +0200

    Add security

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 <html>
 <body style="background-color:#E0218A">
 
-<?php $kakan = $_GET['kakan']; $kakan++; ?>
+<?php $kakan_url = base64_decode($_GET['kakan']); $kakan_url++; ?>
 
 <div style="display: flex; justify-content: center; height: 80%; position: relative">
-    <a href="?<?php echo "kakan=$kakan" ?>">
+    <a href="?<?php echo "kakan=" . base64_encode($kakan_url) ?>">
         <img src="<?php echo "img/1.png"; ?>" alt="Girl in a jacket">
     </a>
 </div>
 
 <div style="display: flex; justify-content: center; height: 20%; font-size: 100px; position: relative">
-    <?php echo --$kakan;?>
+    <?php echo --$kakan_url;?>
 </div>
 
 </body>
